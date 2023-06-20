9f3459c66a7c1481e5200ee50af46b907a0154b5
yuyanakao1121 <kaihatsu.yuya.nakao@gmail.com>
2023-06-20 16:10:54 +0900

    up

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,17 @@
-<script>
-import axios from 'axios';
+<template>
+  <div>
+    <h2>投票アプリ</h2>
+    <form netlify>
+      <input type="text" v-model="name" placeholder="名前" name="name" />
+      <select v-model="selectedOption" name="selectedOption">
+        <option v-for="option in options" :key="option">{{ option }}</option>
+      </select>
+      <button type="submit">投票</button>
+    </form>
+  </div>
+</template>
 
+<script>
 export default {
   data() {
     return {
@@ -9,32 +20,5 @@ export default {
       options: ['選択肢1', '選択肢2', '選択肢3', '選択肢4'],
     };
   },
-  methods: {
-    async vote() {
-      try {
-        const response = await axios.post('/.netlify/functions/vote', {
-          name: this.name,
-          selectedOption: this.selectedOption,
-        });
-
-        const { totalVotes, winner } = response.data;
-        // 結果の表示など、必要な処理を追加
-      } catch (error) {
-        console.error(error);
-      }
-    },
-  },
 };
 </script>
-
-
-<template>
-  <div>
-    <h2>投票アプリ</h2>
-    <input type="text" v-model="name" placeholder="名前" />
-    <select v-model="selectedOption">
-      <option v-for="option in options" :key="option">{{ option }}</option>
-    </select>
-    <button @click="vote">投票</button>
-  </div>
-</template>
