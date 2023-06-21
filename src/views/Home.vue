001b704db9cc2e21791d5e697d891c03feaeca16
yuyanakao1121 <kaihatsu.yuya.nakao@gmail.com>
2023-06-21 09:54:42 +0900

    up

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,79 +1,57 @@
 <template>
   <div class="container">
-    <form @submit="submitForm" class="contact_form" name="contact" method="POST" data-netlify="true">
+    <form class="contact_form" name="contact" method="POST" data-netlify="true" action="">
       <input type="hidden" name="form-name" value="contact" />
       <div class="contact_item">
         <label for="name">お名前</label>
-        <input type="text" id="name" v-model="name" name="name" autocomplete="name" />
+        <input type="text" id="name" name="name" autocomplete="name" />
       </div>
       <div class="contact_item">
         <label for="email">メールアドレス</label>
-        <input type="email" id="email" v-model="email" name="email" autocomplete="email" />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          autocomplete="email"
+        />
       </div>
       <div class="contact_item">
         <label for="message">お問い合わせ内容</label>
-        <textarea id="message" v-model="message" rows="12" name="message"></textarea>
+        <textarea id="message" rows="12" name="message"></textarea>
       </div>
       <div class="contact_submit">
         <button type="submit">送信</button>
       </div>
     </form>
-    <div v-if="submitted" class="confirmation_message">
-      参加ありがとうございます！
-    </div>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
+  data () {
     return {
-      name: '',
-      email: '',
-      message: '',
-      submitted: false
-    };
-  },
-  methods: {
-    submitForm(event) {
-      event.preventDefault();
-      console.log('フォームが送信されました');
-      console.log('名前:', this.name);
-      console.log('メールアドレス:', this.email);
-      console.log('お問い合わせ内容:', this.message);
-      // フォームの送信処理を追加する場合はここに記述します
-
-      this.submitted = true; // 送信完了フラグを立てる
     }
   }
-};
+}
 </script>
 
-<style>
+<style scoped>
 .container {
-  max-width: 600px;
+  max-width: 800px;
   margin: 0 auto;
-  padding: 20px;
 }
-
-label {
-  display: block;
-  margin-bottom: 5px;
+.contact_form .contact_item {
+  display: flex;
+  margin-bottom: 2%;
 }
-
-input,
-textarea {
-  width: 100%;
-  padding: 10px;
-  margin-bottom: 10px;
+.contact_form .contact_item label {
+  width: 20%;
 }
-
-button {
-  padding: 10px 20px;
+.contact_form .contact_item input,
+.contact_form .contact_item textarea {
+  width: 80%;
 }
-
-.confirmation_message {
-  color: red;
-  margin-top: 10px;
+.contact_form .contact_submit {
+  text-align: center;
 }
 </style>
