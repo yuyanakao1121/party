7c6052f96343cfacc9637c5338b5cd3e590488dc
yuyanakao1121 <kaihatsu.yuya.nakao@gmail.com>
2023-06-21 10:19:58 +0900

    うｐ

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,57 +1,29 @@
 <template>
   <div class="container">
-    <form class="contact_form" name="contact" method="POST" data-netlify="true" action="">
+    <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
       <input type="hidden" name="form-name" value="contact" />
-      <div class="contact_item">
-        <label for="name">お名前</label>
-        <input type="text" id="name" name="name" autocomplete="name" />
-      </div>
-      <div class="contact_item">
-        <label for="email">メールアドレス</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          autocomplete="email"
-        />
-      </div>
-      <div class="contact_item">
-        <label for="message">お問い合わせ内容</label>
-        <textarea id="message" rows="12" name="message"></textarea>
-      </div>
-      <div class="contact_submit">
-        <button type="submit">送信</button>
-      </div>
+      <input id="name" name="name" type="text" placeholder="お名前" required />
+      <input id="email" name="email" type="email" placeholder="メールアドレス" required />
+      <textarea id="message" name="message" placeholder="お問い合わせ内容" required></textarea>
+      <button type="submit">送信</button>
     </form>
+    <p v-if="submitted">ありがとう！</p>
   </div>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
-    }
+      submitted: false
+    };
   }
-}
+};
 </script>
 
 <style scoped>
 .container {
   max-width: 800px;
   margin: 0 auto;
-}
-.contact_form .contact_item {
-  display: flex;
-  margin-bottom: 2%;
-}
-.contact_form .contact_item label {
-  width: 20%;
-}
-.contact_form .contact_item input,
-.contact_form .contact_item textarea {
-  width: 80%;
-}
-.contact_form .contact_submit {
-  text-align: center;
 }
 </style>
